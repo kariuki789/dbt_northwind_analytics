ec9edea3b35e0474500396367e8382ae4fc39fe6
Morris Mwaura <kariuki538@gmail.com>
2026-05-18 09:50:27 +0000

    done

--- models/Production/customers.sql
+++ models/Production/customers.sql
@@ -2,10 +2,10 @@
 
 select
     *,
-    CASE WHEN region_group = 'Europe' THEN true  ELSE false END AS is_european,
+    case when region_group = 'Europe' then true else false end as is_european,
     case
-        WHEN country IN ('Germany', 'USA', 'Brazil', 'France')
-        THEN 'Key Market'
-        ELSE 'Standard'
-    END AS customer_tier
+        when country in ('Germany', 'USA', 'Brazil', 'France')
+        then 'Key Market'
+        else 'Standard'
+    end as customer_tier
 from {{ ref("stg_customers") }}
